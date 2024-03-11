27ea3a3ba0a190e66ee42fafcddbfa8cebd1d531
Chema Lopez <chema.lopezp@gmail.com>
2024-03-11 15:42:16 -0700

    Import starter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-const App = () => {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
-  );
-};
+import "./App.css";
+
+function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  // Score
+  // Rules
+
+  // You Picked
+  // The House Picked
+
+  // You Win
+  // You Lose
+
+  // Play Again
+}
 
 export default App;
